e0a89fb67af5c53a28cb3bab0082fe505458e556
Enrique Aguilar <enriqueao96@gmail.com>
2017-03-25 07:50:45 -0600

    estadisticas

---
+++ views/estadisticas.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Estadisticas</title>
+</head>
+<body>
+<!-- //num de estadisticas
+//sobre dias
+//alerta mas constante
+//hora más ocurrente
+//zona, municicio, estado -->
+	<ul>
+		<li>Cantidad de registros</li>
+		<li>Dias</li>
+		<li>Horas mas concurrentes</li>
+		<li>Municipio y estado con mas concurrencias</li>
+		<li>zona con mas concurrencias</li>
+	</ul>
+</body>
+</html>
