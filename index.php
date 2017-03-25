4c5c25a0906835ccdf011522da872cd8c0567004
Fernando Ortiz Rico Celio <fernandorc@79-1-95-178.pool.ukrtel.net>
2017-03-24 19:39:32 -0600

    Falta terminar pantalla de log in

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="white">
         <link href="public/css/style.css" rel="stylesheet" type="text/css">
-        <script type="text/javascript" src="js/magic.js"></script>
+        <script type="text/javascript" src="public/js/magic.js"></script>
     </head>
     <body>
         
@@ -16,16 +16,24 @@
             <input type="text" id="userLogIn">
             <input type="password" id="passwordLogIn">
             
-            <button id="btnLogIn">Iniciar Sesión</button>
+            <button id="btnLogIn" class="mainButton">Iniciar Sesión</button>
             
             <div>
-                <button class="btnSecond">Crear Usuario</button>
+                <button class="btnSecond" onclick="showRegistro()">Crear Usuario</button>
                 <button class="btnSecond">Recuperar Contraseña</button>
             </div>
             
         </div>
         
-        <div class="downDiv">
+        <div class="downDiv" id="signUp">
+            
+            <input type="text" id="userSignUp">
+            <input type="email" id="emailSignUp">
+            <input type="number" id="phoneSignUp">
+            <input type="password" id="passwordSignUp">
+            
+            <button class="mainButton">Crear Usuario</button>
+            <button class="btnOne">Ya tengo cuenta</button>
             
         </div>
         
---
+++ public/js/magic.js
@@ -0,0 +1,7 @@
+function showRegistro() {
+    document.getElementById('signIn').classList.remove('centerDiv');
+    document.getElementById('signIn').classList.add('upDiv');
+    
+    document.getElementById('signUp').classList.remove('downDiv');
+    document.getElementById('signUp').classList.add('centerDiv');
+}
