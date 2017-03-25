0ab62e6301d9316224b790bf7be0e87734b9b0f5
Fernando Ortiz Rico Celio <fernandorc@MacBook-Air-de-Fernando.local>
2017-03-25 03:32:53 -0600

    Jaj

---
+++ views/reporte.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Securitas</title>   
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="white">
+        <link href="../public/css/style.css" rel="stylesheet" type="text/css">
+        <link href="../public/css/sweetalert.css" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="../public/js/magic.js"></script>
+        <script type="text/javascript" src="../public/js/sweetalert.min.js"></script>
+    </head>
+    <body>
+       
+       <div class="contenedor">
+           <h1>Tipo de Reporte</h1>
+           
+           <div id="grave"><img src="../public/media/gun.svg">Grave</div>
+           <div id="moderado"><img src="../public/media/knife.svg">Moderado</div>
+           <div id="leve"><img src="../public/media/fist.svg">Leve</div>
+           
+       </div>
+        
+        
+        <div class="backImage"></div>
+    </body>
+</html>
