14acf1543c7a29465b84d7d9a4848f95b18fbd9d
Enrique Aguilar <enriqueao96@gmail.com>
2017-03-25 11:03:00 -0600

    icon app

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="white">
+        <link rel="shortcut icon" type="image/png" href="icon.png">
+
         <link href="public/css/style.css" rel="stylesheet" type="text/css">
          <script type="text/javascript" src="public/js/sweetalert.min.js"></script>
           <link href="public/css/sweetalert.css" rel="stylesheet" type="text/css">
