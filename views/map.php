80cae04a8cc2fee8e37c143303c91c70791793d7
Fernando Ortiz Rico Celio <fernandorc@79-1-95-178.pool.ukrtel.net>
2017-03-24 20:13:16 -0600

    Estilos agregados en login

---
+++ views/map.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Securitas</title>   
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="white">
+        <link href="public/css/style.css" rel="stylesheet" type="text/css">
+        <script type="text/javascript" src="../public/js/magic.js"></script>
+    </head>
+    <body>
+        
+        
+        
+    </body>
+</html>
